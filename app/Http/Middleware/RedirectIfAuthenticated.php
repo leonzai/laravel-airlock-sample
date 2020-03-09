55dcc1b80f38e51e22109a7011188922aa42872b
leonzai <leonzai@aliyun.com>
2020-03-09 16:36:55 +0800

    optimize

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            return response()->json([
+                'message'=> __("You are Authenticated!")
+            ]);
         }
 
         return $next($request);
--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['login', 'api/*', 'airlock/csrf-cookie'],
+    'paths' => ['register', 'login', 'logout', 'api/*', 'airlock/csrf-cookie'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
@@ -56,5 +56,5 @@ return [
     /*
      * Sets the Access-Control-Allow-Credentials header.
      */
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
