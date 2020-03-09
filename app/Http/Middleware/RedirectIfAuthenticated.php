8f347c23c1fd2b80e873afd162ca5ecfcca45d1b
leonzai <leonzai@aliyun.com>
2020-03-09 13:48:23 +0800

    fix RedirectIfAuthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -11,15 +11,17 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            return response()->json([
+                'message'=> __("You are Authenticated!")
+            ]);
         }
 
         return $next($request);
